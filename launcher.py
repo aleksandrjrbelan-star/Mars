82a147c293ec251a9562ee6d105335461172f1d1
RaLoPus <aleksandr.jr.belan@gmail.com>
2026-06-22 18:09:34 +0300

    Launcher

--- launcher.py
+++ launcher.py
@@ -1,27 +1,52 @@
 from customtkinter import *
 from tkinter import messagebox
+from PIL import Image
 import subprocess
 import sys
 
 window = CTk()
 window.title("Mars")
-window.geometry("500x500")
+window.geometry("700x600")
 window.resizable(False, False)
 
-title = CTkLabel(window, text="Mars doesn`t give you a second chance", font=CTkFont(size=24, weight="bold"))
+bg_image = Image.open("data/images/background/launcher/launcher_bg.png")
+bg = CTkImage(bg_image, size=(700, 600))
+bg_label = CTkLabel(window, image=bg,text="")
+bg_label.place(x=0, y=0)
+
+combobox = CTkComboBox(
+    window,
+    values=["Одиночна гра", "Багатокористувацька гра"],
+    
+    # --- НАЛАШТУВАННЯ КОЛЬОРІВ ---
+    bg_color="transparent",              # Фон самого віджета (прозорий)
+    fg_color="#1a1a1a",          # Колір самого поля (темно-сірий)
+    border_color="#FF4500",      # Колір рамки (марсіанський помаранчевий)
+    button_color="#FF4500",      # Колір стрілочки вибору
+    button_hover_color="#cc3700",# Колір стрілочки при наведенні
+    text_color="white",          # Колір тексту в полі
+    dropdown_text_color="white", # Колір тексту у випадаючому списку
+    dropdown_fg_color="#2b2b2b", # Фон самого випадаючого списку
+    dropdown_hover_color="#FF4500", # Колір рядка, на який навели мишку
+    state="readonly",
+    width=250
+)
+combobox.place(x=11,y=466)
 
 def start_game():
     try:
-        window.withdraw()
-        subprocess.run([sys.executable, "main.py"])
-        window.deiconify()
+        if combobox.get() == "Багатокористувацька гра":
+            messagebox.showinfo("Інформація", "Багатокористувацька гра ще не реалізована.")
+            return
+        else:
+            window.withdraw()
+            subprocess.run([sys.executable, "singleplayer.py"])
+            window.deiconify()
     except Exception as e:
         messagebox.showerror("Помилка", f"Сталася помилка при запуску гри:\n{e}")
-start_button = CTkButton(window, text="Start", font=CTkFont(size=30, weight="bold"), width=200, height=50, command=start_game)
+start_button = CTkButton(window, text="Start", font=CTkFont(size=30, weight="bold"), width=300, height=79, command=start_game)
 
-title.pack(pady=10)
-
-start_button.pack(pady=200)
+start_button.place(x=11, y=508)
 
 
 window.mainloop()
